e759ee7345ba577956a1ef6e0adbe92826eaa182
Anton985 <83685086+Anton985@users.noreply.github.com>
2022-03-27 21:10:38 +1000

    Add files via upload

---
+++ HomeWork2.java
@@ -0,0 +1,43 @@
+/**
+  * Java 1. HomeWork 2
+  *
+  * @author Anton
+  * version 27.03.2022
+  */
+  
+  public class HomeWork2 {
+	  
+	  public static void main(String[] args) {
+		  System.out.println(intervalOfNumbers(12, 9));
+		  integerNumber(10);
+		  System.out.println(comparison(5));
+		  stringsAndNumbers("exercise 4", 10);
+	    }
+	    
+		static boolean intervalOfNumbers(int a, int b) {
+			int sum = a + b;
+                if (sum >= 10 && sum <=20) {
+                    return true;
+            }
+                    return false;			
+		}
+		
+		static void integerNumber(int x) {
+			if (x >= 0) {
+				System.out.println("positive number");
+			} else {
+				System.out.println("Negative number");
+			}
+		}		
+			
+		static boolean comparison(int a) {
+			return a <= 0;
+		} 				
+		
+		static void stringsAndNumbers(String str, int n) {
+			for (int i = 1; i <= n; i++) {
+				System.out.println(str);
+			}
+		}
+	}
+  
